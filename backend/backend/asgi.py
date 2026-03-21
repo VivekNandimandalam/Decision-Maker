f62625168a61f6a051af075d044427d092808caf
VivekNandimandalam <viveknandimandalam463@gmail.com>
2026-03-21 21:27:19 +0000

    Fix ASGI settings initialization order

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -12,12 +12,9 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 from polls.routing import websocket_urlpatterns
-
-
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_app = get_asgi_application()
 
